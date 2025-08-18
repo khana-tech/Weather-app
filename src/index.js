d6296df437eb3fdc6c784cf1c63c87e3ab68e691
khana-tech <nyakanpeter66@gmail.com>
2025-08-19 01:27:39 +0300

    added search functionalities using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city-details");
-    cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperatue");
+  let temperatue = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperatue);
+   let cityElement = document.querySelector("#city-details");
+   cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "2d2b3bf3aa9770ff0e33400b5b8o403t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+ 
+  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("paris")
